6a362273c5e1018847e471479f0a8e4ede2b6cd4
Rajat <rajatprajapati09@gmail.com>
2023-03-12 01:50:04 +0530

    Creadted a class with Fields and list of that class

---
+++ ProductReview.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Product_Review_Management
+{
+    public class ProductReview
+    {
+        public int ProductId;
+        public int UserId;
+        public double Rating;
+        public string Review;
+        public bool isLike;
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Product_Review_Management
 {
@@ -7,6 +8,22 @@ namespace Product_Review_Management
         static void Main(string[] args)
         {
             Console.WriteLine("Product Review Management");
+            List<ProductReview> productReviews = new List<ProductReview>() 
+            {
+                new ProductReview() { ProductId = 1, UserId = 1, Rating = 5, Review = "Good", isLike = true },
+                new ProductReview() { ProductId = 2, UserId = 2, Rating = 4, Review = "Good", isLike = true },
+                new ProductReview() { ProductId = 3, UserId = 3, Rating = 4, Review = "Good", isLike = true },
+                new ProductReview() { ProductId = 4, UserId = 4, Rating = 3, Review = "Good", isLike = true },
+                new ProductReview() { ProductId = 5, UserId = 5, Rating = 4, Review = "Good", isLike = true },
+                new ProductReview() { ProductId = 6, UserId = 6, Rating = 4, Review = "Nice", isLike = true },
+                new ProductReview() { ProductId = 7, UserId = 7, Rating = 3, Review = "Good", isLike = true }
+
+            };
+            foreach (ProductReview Pr in productReviews)
+            {
+                Console.WriteLine("ProductId: "+ Pr.ProductId +" UserId: "+ Pr.ProductId+" Rating: " +Pr.Rating+" Review: "+ Pr.Review +" isLike: "+ Pr.isLike);
+            }
+
         }
     }
 }
