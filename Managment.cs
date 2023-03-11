c2a82769619db6a690a684ea1fedebe03c4f5a74
Rajat <rajatprajapati09@gmail.com>
2023-03-12 02:16:01 +0530

    Top 3 product review by rating

---
+++ Managment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+
+
+namespace Product_Review_Management
+{
+    public class Managment
+    {
+       
+          
+        public void TopRecords(List<ProductReview>  ListProductreviews) 
+        {
+            var recordedData = (from productReview in ListProductreviews
+                                orderby productReview.Rating descending
+                                select productReview).Take(3);
+            foreach (var Pr in recordedData)
+            {
+                Console.WriteLine("ProductId: " + Pr.ProductId + " UserId: " + Pr.ProductId + " Rating: " + Pr.Rating + " Review: " + Pr.Review + " isLike: " + Pr.isLike);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,20 +10,19 @@ namespace Product_Review_Management
             Console.WriteLine("Product Review Management");
             List<ProductReview> productReviews = new List<ProductReview>() 
             {
-                new ProductReview() { ProductId = 1, UserId = 1, Rating = 5, Review = "Good", isLike = true },
-                new ProductReview() { ProductId = 2, UserId = 2, Rating = 4, Review = "Good", isLike = true },
-                new ProductReview() { ProductId = 3, UserId = 3, Rating = 4, Review = "Good", isLike = true },
-                new ProductReview() { ProductId = 4, UserId = 4, Rating = 3, Review = "Good", isLike = true },
-                new ProductReview() { ProductId = 5, UserId = 5, Rating = 4, Review = "Good", isLike = true },
-                new ProductReview() { ProductId = 6, UserId = 6, Rating = 4, Review = "Nice", isLike = true },
-                new ProductReview() { ProductId = 7, UserId = 7, Rating = 3, Review = "Good", isLike = true }
+                new ProductReview() { ProductId = 1, UserId = 1, Rating = 5,   Review = "Good", isLike = true },
+                new ProductReview() { ProductId = 2, UserId = 2, Rating = 4.5, Review = "Good", isLike = true },
+                new ProductReview() { ProductId = 3, UserId = 3, Rating = 4,   Review = "Good", isLike = true },
+                new ProductReview() { ProductId = 4, UserId = 4, Rating = 3,   Review = "Good", isLike = true },
+                new ProductReview() { ProductId = 5, UserId = 5, Rating = 4.2, Review = "Good", isLike = true },
+                new ProductReview() { ProductId = 6, UserId = 6, Rating = 4.1, Review = "Nice", isLike = true },
+                new ProductReview() { ProductId = 7, UserId = 7, Rating = 3,   Review = "Good", isLike = true }
 
             };
-            foreach (ProductReview Pr in productReviews)
-            {
-                Console.WriteLine("ProductId: "+ Pr.ProductId +" UserId: "+ Pr.ProductId+" Rating: " +Pr.Rating+" Review: "+ Pr.Review +" isLike: "+ Pr.isLike);
-            }
+         
 
+            Managment managment = new Managment();
+            managment.TopRecords(productReviews);
         }
     }
 }
